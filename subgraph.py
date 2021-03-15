3aac0d9589f5fcd15edde5f2ff9894901956e0b9
jintao <hellojintao@protonmail.com>
2021-03-15 19:11:59 -0400

    add bsc final vaults

--- subgraph.py
+++ subgraph.py
@@ -1,17 +1,29 @@
 from jinja2 import Template
 from json import load
+import sys
+
+supported_chains = ['mainnet', 'bsc']
 
 if __name__ == '__main__':
-  with open('setts.json') as j:
+  chain = 'mainnet'
+  if len(sys.argv) > 1:
+    chain = sys.argv[1]
+  if chain not in supported_chains:
+    raise Exception('invalid chain')
+
+  with open(chain + '/setts.json') as j:
     settContracts = load(j)
   setts = [ value for key, value in settContracts.items() ]
 
-  with open('geysers.json') as j:
-    geyserContracts = load(j)
-  geysers = [ value for key, value in geyserContracts.items() ]
+  try:
+    with open(chain + '/geysers.json') as j:
+      geyserContracts = load(j)
+    geysers = [ value for key, value in geyserContracts.items() ]
+  except:
+    geysers = []
 
   with open('subgraph.j2') as s:
       template = Template(s.read())
   
-  with open('subgraph.yaml', 'w') as t:
-    t.write(template.render(setts=setts, geysers=geysers))
+  with open(chain + '/subgraph.yaml', 'w') as t:
+    t.write(template.render(chain=chain, setts=setts, geysers=geysers))
