a9578d10dac501ac15c35376747ad887dae57011
jintao <hellojintao@protonmail.com>
2021-08-10 13:14:27 -0500

    chore: update from + to field

--- src/handlers/sett-handler.ts
+++ src/handlers/sett-handler.ts
@@ -10,8 +10,8 @@ import { depositBalance, loadUserBalance, withdrawBalance } from '../entities/us
 
 export function handleTransfer(event: Transfer): void {
   let timestamp = event.block.timestamp.toI32();
-  let from = event.params.to;
-  let to = event.params.from;
+  let from = event.params.from;
+  let to = event.params.to;
   let value = event.params.value;
   handleSettTokenTransfer(timestamp, event.address, from, to, value, true);
 }
